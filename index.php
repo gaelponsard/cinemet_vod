cefc1d0cad0ffb77b28591484b205f054d69dbe6
gaelponsard <g.ponsard@simplon-charleville.fr>
2019-04-28 21:56:44 +0200

    mise en place du slider

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Start WOWSlider.com HEAD section -->
+<link rel="stylesheet" type="text/css" href="slider_home/engine1/style.css" />
+<script type="text/javascript" src="slider_home/engine1/jquery.js"></script>
+<!-- End WOWSlider.com HEAD section -->
+    <link rel="stylesheet" href="styles.css">
+    <title>Cinemet V.O.D.</title>
+</head>
+
+<body>
+<!-- Start WOWSlider.com BODY section -->
+<div id="wowslider-container1">
+<div class="ws_images"><ul>
+		<li><a href="#" target="_self"><img src="slider_home/data1/images/01.jpg" alt="Avengers: Endgame" title="Avengers: Endgame" id="wows1_0"/></a>Phase finale pour les vengeurs...</li>
+		<li><a href="#"><img src="slider_home/data1/images/02.jpg" alt="Alita: Battle Angel" title="Alita: Battle Angel" id="wows1_1"/></a>Le chef d'oeuvre de Cameron et Rodriguez !</li>
+		<li><a href="#"><img src="slider_home/data1/images/03.jpg" alt="John Wick Parabellum" title="John Wick Parabellum" id="wows1_2"/></a>Sa tête est mise à prix !</li>
+		<li><a href="#"><img src="slider_home/data1/images/04.jpg" alt="Dumbo" title="Dumbo" id="wows1_3"/></a>Quand Burton retrouve Disney</li>
+		<li><a href="#" target="_self"><img src="slider_home/data1/images/05.jpg" alt="Notre sélection 90's" title="Notre sélection 90's" id="wows1_4"/></a>Un bon cru à découvrir !</li>
+	</ul></div>
+	<div class="ws_bullets"><div>
+		<a href="#" title="Avengers: Endgame"><span><img src="slider_home/data1/tooltips/01.jpg" alt="Avengers: Endgame"/>1</span></a>
+		<a href="#" title="Alita: Battle Angel"><span><img src="slider_home/data1/tooltips/02.jpg" alt="Alita: Battle Angel"/>2</span></a>
+		<a href="#" title="John Wick Parabellum"><span><img src="slider_home/data1/tooltips/03.jpg" alt="John Wick Parabellum"/>3</span></a>
+		<a href="#" title="Dumbo"><span><img src="slider_home/data1/tooltips/04.jpg" alt="Dumbo"/>4</span></a>
+		<a href="#" title="Notre sélection 90's"><span><img src="slider_home/data1/tooltips/05.jpg" alt="Notre sélection 90's"/>5</span></a>
+	</div></div><div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.net">jquery slider</a> by WOWSlider.com v8.8</div>
+<div class="ws_shadow"></div>
+</div>	
+<script type="text/javascript" src="slider_home/engine1/wowslider.js"></script>
+<script type="text/javascript" src="slider_home/engine1/script.js"></script>
+<!-- End WOWSlider.com BODY section -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>-->
+</body>
+
+</html>
