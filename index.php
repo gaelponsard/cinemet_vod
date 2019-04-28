c8797672c45d6b31e57a65e578b84264262cdca5
gaelponsard <g.ponsard@simplon-charleville.fr>
2019-04-28 22:48:44 +0200

    ajout navbar temporaire

--- index.php
+++ index.php
@@ -16,7 +16,45 @@
 </head>
 
 <body>
-<!-- Start WOWSlider.com BODY section -->
+<!-- début navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Cinemet V.O.D.</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<!-- fin navbar -->
+
+<!-- début WOWSlider section -->
 <div id="wowslider-container1">
 <div class="ws_images"><ul>
 		<li><a href="#" target="_self"><img src="slider_home/data1/images/01.jpg" alt="Avengers: Endgame" title="Avengers: Endgame" id="wows1_0"/></a>Phase finale pour les vengeurs...</li>
@@ -36,7 +74,7 @@
 </div>	
 <script type="text/javascript" src="slider_home/engine1/wowslider.js"></script>
 <script type="text/javascript" src="slider_home/engine1/script.js"></script>
-<!-- End WOWSlider.com BODY section -->
+<!-- fin WOWSlider  section -->
 
 
 
