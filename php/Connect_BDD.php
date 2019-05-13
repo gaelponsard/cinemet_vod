c36cd455d382dd1d65bf527f8bbf032c5ccd3b5f
1 <g.ponsard@simplon-charleville.fr>
2019-05-13 14:08:56 +0200

    petites retouches

--- php/Connect_BDD.php
+++ php/Connect_BDD.php
@@ -2,7 +2,7 @@
 
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=metropolis;charset=utf8', 'root', '');
+	$bdd = new PDO('mysql:host=localhost;dbname=allocinemet;charset=utf8', 'commun', 'commun');
 }
 catch (PDOexception $e)
 {
