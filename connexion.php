78cde4a12292cbb115786b28b887294196b15da6
gaelponsard <g.ponsard@simplon-charleville.fr>
2019-05-18 13:42:15 +0200

    page inscription  test OK + début page profil

---
+++ connexion.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Connexion</title>
+</head>
+<body>
+    <!---------CONNECTION A LA BDD --------->
+<?php include('php/Connect_BDD.php');
+
+
+
+//////////////////////////////  NAVBAR  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+include('header.php');
+</body>
+</html>
